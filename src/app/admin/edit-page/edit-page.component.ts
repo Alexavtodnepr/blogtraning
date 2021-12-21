d2d3fcf2819d5e1866bcd2867aebc743cbd3db0b
Alexavtodnepr <82681866+Alexavtodnepr@users.noreply.github.com>
2021-12-22 01:22:56 +0200

    Add files via upload

---
+++ src/app/admin/edit-page/edit-page.component.ts
@@ -0,0 +1,61 @@
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {ActivatedRoute, Params} from "@angular/router";
+import {PostsService} from "../../shared/posts.service";
+import {Subscription, switchMap} from "rxjs";
+import {Post} from "../../shared/interfaces";
+import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {AlertService} from "../shared/services/alert.service";
+
+@Component({
+  selector: 'app-edit-page',
+  templateUrl: './edit-page.component.html',
+  styleUrls: ['./edit-page.component.scss']
+})
+export class EditPageComponent implements OnInit, OnDestroy {
+  form: FormGroup | any;
+  post: Post | any;
+  submitted = false;
+  ubSub: Subscription | undefined;
+
+
+  constructor(
+    private route: ActivatedRoute,
+    private postsService: PostsService,
+    private alert: AlertService
+  ) { }
+
+  ngOnInit(): void {
+    this.route.params.pipe(
+      switchMap((params: Params) => {
+        return this.postsService.getById(params['id'])
+    })
+    ).subscribe((post:Post) => {
+      this.post = post;
+      this.form = new FormGroup({
+        title: new FormControl(post.title, Validators.required),
+        text: new FormControl(post.text, Validators.required)
+      })
+    })
+  }
+
+  ngOnDestroy() {
+    if(this.ubSub){
+      this.ubSub.unsubscribe();
+    }
+  }
+
+  submit() {
+    if (this.form.invalid){
+      return
+    }
+    this.submitted = true;
+    this.ubSub = this.postsService.update({
+      ...this.post,
+      text: this.form.value.text,
+      title: this.form.value.title,
+    }).subscribe(() => {
+      this.submitted = false;
+      this.alert.success('Post was updated!')
+    })
+  }
+}
